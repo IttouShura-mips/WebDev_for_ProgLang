711ab8b67629532c6559e5798cf646e6fb3b8685
Shanrayeguzman0 <shanrayeguzman0@gmail.com>
2026-08-10 09:57:06 +0800

    FIX ATTEMPT

--- backend/UsersDB/register.php
+++ backend/UsersDB/register.php
@@ -193,7 +193,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
             <div class="action-group">
                 <button type="submit" name="submit" class="login-button">Sign Up</button>
                 <!-- FIXED: Same-directory path -->
-                <a href="../../student/login.html" class="back-login">Back to Login</a>
+                <a href="../../login.html" class="back-login">Back to Login</a>
             </div>
         </form>
 
